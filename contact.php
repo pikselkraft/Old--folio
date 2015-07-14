4f98076fa307a92789b1376f69bd5adb06592a43
wtod <derek.salmon.fr@gmail.com>
2015-07-14 10:46:07 +0200

    rm images, add all files and modification from production

--- contact.php
+++ contact.php
@@ -1,19 +1,64 @@
 <?php
 /**************************************************************
-* envoi d'un mail + copy *				
+* envoi d'un mail + copy *
 **************************************************************/
-$destinataire = $_POST['email'];
-$name  = $_POST['name'];
-$message  = $_POST['message'];
+
+/****
+recaptcha gestion
+*****/
+
+// grab recaptcha library
+require_once "recaptchalib.php";
+
+// your secret key
+$secret = "6LdEDQcTAAAAAD0nxswITQS7noCaNUGzMr5cyICZ";
+// empty response
+$response = null;
+
+// check secret key
+$reCaptcha = new ReCaptcha($secret);
+
+if ($_POST["g-recaptcha-response"]) {
+    $response = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["g-recaptcha-response"]
+    );
+}
+
+$captcha=$_POST['g-recaptcha-response'];
+
+function secInput($data)
+{
+    $data = stripslashes($data);
+    $data = strip_tags($data);
+    $data = trim($data);
+    $data = htmlentities($data);
+
+    return $data;
+}
+
+$destinataire = secInput($_POST['email']);
+$name         = secInput($_POST['name']);
+$message      = secInput($_POST['message']);
 
 
-$to = 'contact@derek-salmon.me';
-$subject = 'Demande de contact '. $name;
-$msg = $message;
-$headers = 'From: '.$name. ' <'.$destinataire.'>'."\r\n";
-$headers .= 'Bcc: contact <derek.salmon.fr@gmail.com>'."\r\n";
-$headers .= "\r\n";
-mail($to, $subject, $msg, $headers);
+if($_POST['bot'] != '')
+{
+  header('Location:index.php?send=2');
+}
+else if ($response != null && $response->success)
+{
+  		$to      = 'contact@dereksalmon.me';
+  		$subject = 'Demande de contact '. $name;
+  		$msg     = $message;
+  		$headers = 'From: '.$name. ' <'.$destinataire.'>'."\r\n";
+  		$headers .= 'Bcc: contact <derek.salmon.fr@gmail.com>'."\r\n";
+  		$headers .= "\r\n";
+  		mail($to, $subject, $msg, $headers);
 
-header('Location:index.php?send=1');
+  		header('Location:index.php?send=1');
+}
+else {
+  header('Location:index.php?send=3');
+}
 ?>
